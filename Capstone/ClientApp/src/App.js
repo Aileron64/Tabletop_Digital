61f4729fc735173696f1d5fe74d11b5ff95949b4
Thomas Casse <AlexTcp@hotmail.com>
2020-02-18 14:46:08 -0500

    attempting to fix merge errors

--- Capstone/ClientApp/src/App.js
+++ Capstone/ClientApp/src/App.js
@@ -14,7 +14,7 @@ export default class App extends Component
         return (
             <Layout>
                 <Route exact path='/' component={Game} />
-                <Route exact path='/debug' component={DebugLog} />
+                <Route path='/debug' component={DebugLog} />
             </Layout>
         );
     }
